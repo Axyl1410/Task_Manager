5da5d1bbcdfe7acee6e6dd08d32bb5fa75870807
AlexNe <truonggiang190689@gmail.com>
2024-08-21 20:12:28 +0700

    login filter

--- src/main/java/vn/edu/giadinh/tasksmanagement/daos/DBHandler.java
+++ src/main/java/vn/edu/giadinh/tasksmanagement/daos/DBHandler.java
@@ -6,8 +6,12 @@ import java.util.List;
 // DAO
 public interface DBHandler<T, K> {
     T get(K key) throws SQLException;
+
     List<T> getAll() throws SQLException;
+
     void insert(T target) throws SQLException;
+
     void update(T target) throws SQLException;
+
     void delete(T target) throws SQLException;
 }
